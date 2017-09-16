0c4ea457a17f7acbe6b965b18b68cc058ea1561d
Stan Chen <solve153@gmail.com>
2017-09-16 12:35:54 +0800

    Add registration via ajax

--- inc/config.php
+++ inc/config.php
@@ -6,7 +6,12 @@
 
   date_default_timezone_set("Asia/Taipei");
 
+  error_reporting(-1);
+  ini_set('display_errors', 'On');
+
   include_once "classes/DB.php";
+  include_once "classes/Filter.php";
+
   $con = DB::getConnection();
 
 ?>
